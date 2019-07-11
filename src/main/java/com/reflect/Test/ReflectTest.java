c8b691cfafdc1256cca106c0259e9e60119da4e7
fanfurong <1072114800@qq.com>
2019-07-11 21:33:39 +0800

    测试用户

--- src/main/java/com/reflect/Test/ReflectTest.java
+++ src/main/java/com/reflect/Test/ReflectTest.java
@@ -7,6 +7,9 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+/**
+ * 测试反射代码
+ */
 public class ReflectTest {
     public static void main(String[] args) throws Exception {
         Class student = Student.class;
